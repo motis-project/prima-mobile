06647ba7b6db8895d71f3ab9c504e23daec487d6
Felix Gündling <felix.guendling@gmail.com>
2025-01-08 21:53:44 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,17 @@
 	import { type Location } from '$lib/Location';
 	import { Input } from '$lib/components/ui/input';
 	import { t } from '$lib/i18n/translation';
-	import type { Match } from '$lib/openapi';
+	import { plan, type Itinerary, type Match, type PlanData, type PlanResponse } from '$lib/openapi';
 	import { pushState } from '$app/navigation';
 	import { page } from '$app/state';
 	import { Button } from '$lib/components/ui/button';
 	import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down';
-	import { ChevronDown } from 'lucide-svelte';
+	import { ChevronDown, ChevronUp } from 'lucide-svelte';
 	import { Separator } from '$lib/components/ui/separator';
 	import * as Card from '$lib/components/ui/card';
+	import { onMount } from 'svelte';
+	import { lngLatToStr } from '$lib/lngLatToStr';
+	import ItineraryList from './ItineraryList.svelte';
 
 	const urlParams = browser ? new URLSearchParams(window.location.search) : undefined;
 
@@ -33,6 +36,50 @@
 	});
 	let fromItems = $state<Array<Location>>([]);
 	let toItems = $state<Array<Location>>([]);
+
+	const toPlaceString = (l: Location) => {
+		if (l.value.match?.type === 'STOP') {
+			return l.value.match.id;
+		} else if (l.value.match?.level) {
+			return `${lngLatToStr(l.value.match!)},${l.value.match.level}`;
+		} else {
+			return `${lngLatToStr(l.value.match!)},0`;
+		}
+	};
+	let baseQuery = $derived(
+		from.value.match && to.value.match
+			? ({
+					query: {
+						time: new Date().toISOString(),
+						fromPlace: toPlaceString(from),
+						toPlace: toPlaceString(to),
+						timetableView: true
+					}
+				} as PlanData)
+			: undefined
+	);
+
+	type Timeout = ReturnType<typeof setTimeout>;
+	let baseResponse = $state<Promise<PlanResponse>>();
+	let routingResponses = $state<Array<Promise<PlanResponse>>>([]);
+	let searchDebounceTimer: Timeout;
+	$effect(() => {
+		if (baseQuery) {
+			clearTimeout(searchDebounceTimer);
+			searchDebounceTimer = setTimeout(() => {
+				const base = plan<true>(baseQuery).then((response) => response.data);
+				baseResponse = base;
+				routingResponses = [base];
+			}, 400);
+		}
+	});
+
+	let connectionsEl = $state<HTMLDivElement>();
+	onMount(() => {
+		if (connectionsEl) {
+			connectionsEl.scrollTop = 48;
+		}
+	});
 </script>
 
 <div class="h-screen p-2">
@@ -82,108 +129,24 @@
 				</Button>
 			</div>
 			<div class="flex gap-4">
-				<Button class="h-8 grow text-center text-sm font-medium">
-					Depart at Thu, Jan 12, 14:21
+				<Button class="h-8 grow gap-1 text-center text-sm font-medium">
+					Abfahrt Do, Jan 12, 14:21
 					<ChevronDown />
 				</Button>
-				<Button class="h-8  text-center text-sm font-medium">
+				<Button class="h-8  gap-1 text-center text-sm font-medium">
 					All Modes
 					<ChevronDown />
 				</Button>
 			</div>
-			<div class="flex grow flex-col gap-4 overflow-y-auto">
-				<Card.Root class="border-input">
-					<Card.Content class="flex flex-col gap-4 p-4">
-						<div class="flex gap-4">
-							<span>5 hr 23 min</span>
-							<Separator orientation="vertical" />
-							2 Transfers
-						</div>
-						<span>16:00 (Thursday) - 07:58 (Friday)</span>
-						<div class="flex gap-4">
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 1102
-							</div>
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 101
-							</div>
-							<div
-								class="w-32 rounded-md border bg-accent px-2 py-1 text-center text-sm font-semibold"
-							>
-								RB 75
-							</div>
-						</div>
-					</Card.Content>
-				</Card.Root>
-				<Card.Root class="border-input">
-					<Card.Content class="flex flex-col gap-4 p-4">
-						<div class="flex gap-4">
-							<span>5 hr 23 min</span>
-							<Separator orientation="vertical" />
-							2 Transfers
-						</div>
-						<span>16:00 (Thursday) - 07:58 (Friday)</span>
-						<div class="flex gap-4">
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 1102
-							</div>
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 101
-							</div>
-							<div
-								class="w-32 rounded-md border bg-accent px-2 py-1 text-center text-sm font-semibold"
-							>
-								RB 75
-							</div>
-						</div>
-					</Card.Content>
-				</Card.Root>
-				<Card.Root class="border-input">
-					<Card.Content class="flex flex-col gap-4 p-4">
-						<div class="flex gap-4">
-							<span>5 hr 23 min</span>
-							<Separator orientation="vertical" />
-							2 Transfers
-						</div>
-						<span>16:00 (Thursday) - 07:58 (Friday)</span>
-						<div class="flex gap-4">
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 1102
-							</div>
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 101
-							</div>
-							<div
-								class="w-32 rounded-md border bg-accent px-2 py-1 text-center text-sm font-semibold"
-							>
-								RB 75
-							</div>
-						</div>
-					</Card.Content>
-				</Card.Root>
-				<Card.Root class="border-input">
-					<Card.Content class="flex flex-col gap-4 p-4">
-						<div class="flex gap-4">
-							<span>5 hr 23 min</span>
-							<Separator orientation="vertical" />
-							2 Transfers
-						</div>
-						<span>16:00 (Thursday) - 07:58 (Friday)</span>
-						<div class="flex gap-4">
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 1102
-							</div>
-							<div class="w-48 rounded-md border px-2 py-1 text-center text-sm font-semibold">
-								ICE 101
-							</div>
-							<div
-								class="w-32 rounded-md border bg-accent px-2 py-1 text-center text-sm font-semibold"
-							>
-								RB 75
-							</div>
-						</div>
-					</Card.Content>
-				</Card.Root>
+			<div bind:this={connectionsEl} class="flex grow flex-col gap-4 overflow-y-auto">
+				<ItineraryList
+					{baseQuery}
+					{baseResponse}
+					{routingResponses}
+					selectItinerary={(it: Itinerary) => {
+						console.log('select');
+					}}
+				/>
 			</div>
 		</div>
 	{/if}
